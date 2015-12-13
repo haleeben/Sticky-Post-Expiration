7395ec30741d70da7c4468bb9907e9a59401a65d
haleeben <haleeben@gmail.com>
2015-12-13 22:13:47 +1100

    Added all the js value checks and toggle areas sorted out

--- assets/js/sticky-post-expiration.js
+++ assets/js/sticky-post-expiration.js
@@ -1,29 +1,105 @@
 (function($){
 
-    var previous = $('#spe-expiration').val();
+    /**
+     * The code in the file handles the implementation of the Sticky Expiration input datepicker.
+     *
+     * Restores the original value if the user clicks the cancel button.
+     * Clears the input value if the user clicks the clear button
+     * Clears the input value if the user clears the datepicker value and clicks OK
+     *
+     */
 
-    $('#spe-expiration').datepicker({
-        dateFormat: 'yy-mm-dd'
+    var $speExpiration = $( '#spe-expiration' );
+    var $speLabel = $( '#spe-expiration-label' );
+    var $speWrap = $( '#spe-expiration-wrap' );
+
+    /**store the current value to use later if the user clicks the cancel button **/
+    var previous = $speExpiration.val();
+
+
+    /**
+     * Set the datepicker format e.g 01 Jan, 2000
+     */
+    $speExpiration.datepicker({
+        dateFormat: 'dd M, yy'
     });
 
-    $('#spe-edit-expiration, .spe-hide-expiration').click(function(e) {
+
+    /**
+     * Set the initial visibility of the Sticky Expiration area
+     */
+    if ( $('#sticky').is( ':checked' ) ){
+
+        $speWrap.addClass( 'active' );
+        $speWrap.slideDown();
+
+    } else {
+
+        $speWrap.removeClass('active');
+        $speWrap.hide();
+    }
+
+
+    /**
+     * Toggle the Sticky Expires area if the Sticky checkbox is changed
+     */
+    $('#sticky').change( function(){
+
+        $speWrap.toggleClass( 'active' );
+
+        if( $speWrap.hasClass( 'active' ) === false ) {
+
+            $speWrap.slideUp();
+            $speExpiration.val('');
+            $speLabel.text( "Never" );
+
+        } else if( $speWrap.hasClass( 'active' ) === true ) {
+
+            $speWrap.slideDown();
+        }
+    });
+
+
+    /**
+     * If the user clicks the clear button, then clear the values including the previous
+     */
+    $('.spe-hide-expiration.clear').click( function( e ) {
+
+        $speExpiration.val('');
+        $speLabel.text( "Never" );
+        previous = $speExpiration.val();
+
+    });
+
+
+    /**
+     * If the Edit or Cancel or OK buttons are clicked do this
+     **/
+    $('#spe-edit-expiration, .spe-hide-expiration').click( function( e ) {
 
         e.preventDefault();
 
-        var date = $('#spe-expiration').val();
+        // Set the current input value to a variable
+        var date = $speExpiration.val();
 
-        if( $(this).hasClass('cancel') ) {
+        // If the user clicks the cancel button restore the previous value
+        if( $( this ).hasClass( 'cancel' ) ) {
 
-            $('#spe-expiration').val( previous );
+            $speExpiration.val( previous );
+            $speLabel.text( $speExpiration.val() );
 
-        } else if( date ) {
+        } else if( date ) { // if there is a value, set the label and previous values
 
-            $('#spe-expiration-label').text( $('#spe-expiration').val() );
+            $speLabel.text( $speExpiration.val() );
+            previous = $speExpiration.val();
 
+        } else { // if there isn't a value and the user didn't the cancel button
+
+            $speExpiration.val('');
+            $speLabel.text( "Never" );
         }
 
-        $('#spe-expiration-field').slideToggle();
-
+        $('#spe-expiration-field').slideToggle(); // Show the input field
     });
 
 })(jQuery);
