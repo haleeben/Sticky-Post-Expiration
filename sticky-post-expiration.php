392ca435454ce28bf9ec1de03d3a798fec2a33d9
haleeben <haleeben@gmail.com>
2015-12-13 19:56:34 +1100

    Made a change to the instance args, added $plugin_file argument to use in the other files

--- sticky-post-expiration.php
+++ sticky-post-expiration.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Plugin Name:   Sticky Post Expiration
+/* Plugin Name:  Sticky Post Expiration
  * Plugin URI:   http://ebenhale.com/sticky-post-expiration
  * Version:      1.0.0
  * Description:  This WordPress plugin allows the user to set a sticky post expiration date
@@ -8,14 +8,23 @@
  * Author URI:   http://ebenhale.com/
  * License:      GPL-2.0+
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ *
  */
 
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/*
+ * Exit if called directly.
+ */
+if ( ! defined( 'WPINC' ) ) die;
 
 
 require_once 'includes/class-sticky-post-expiration.php';
 
 
 
-Sticky_Post_Expiration::get_instance( '1.0.0' , plugin_dir_path(__file__) , plugin_dir_url( __FILE__ ) );
+Sticky_Post_Expiration::instance(
+    '1.0.0' ,
+    plugin_dir_path(__FILE__) ,
+    plugin_dir_url(__FILE__),
+    plugin_basename(__DIR__).'.php'
+);
 
